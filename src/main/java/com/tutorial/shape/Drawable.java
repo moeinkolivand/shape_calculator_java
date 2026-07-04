0342b016649b8bd83188b56af80ddf42dcdee7a8
moein <moeinkolivand97@gmail.com>
2026-07-04 18:10:30 +0330

    add abstract class shape and interface drawable

---
+++ src/main/java/com/tutorial/shape/Drawable.java
@@ -0,0 +1,5 @@
+package com.tutorial.shape;
+
+public interface Drawable {
+    String drawAscii();
+}
---
+++ src/main/java/com/tutorial/shape/Shape.java
@@ -0,0 +1,13 @@
+package com.tutorial.shape;
+
+
+public abstract class Shape {
+    protected String name;
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    public void describe() {
+        System.out.println("The Shape Name Is " + name + "The Area Is " + area() + "And The Perimeter Is " + perimeter());
+    }
+}
